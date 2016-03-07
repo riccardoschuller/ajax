46b116e2572eacab4f086607f0f0d662f465171f
riccardoschuller <cartuciolo@hotmail.it>
2016-03-07 11:43:24 +0100

    aggiunti articoli

--- scripts/main.js
+++ scripts/main.js
@@ -36,9 +36,46 @@ $(document).ready(function () {
 					console.log(request);
 				}			
 			});
+
+	})
+	console.log($("#col-lg-6").length)
+	})
+
+	$("#start2").click(function(){
+
+	$("#second").slideUp();
+
+	$.get('./html/posts.html', function ( userTemplate ) {
+
+		$.ajax( 
+			{
+				url: root + '/posts',
+				method: 'GET', //GET, POST, PUT, DELETE
+				contentType: 'application/json', //ask server to return json
+				dataType: 'json', //says to server we are sending json
+				//Call if request return successfully
+				success: function (response) {
+
+					var template = Handlebars.compile($(userTemplate)[0].outerHTML); //convert from jquery to string
+
+					for(var i = 0; i < 5; i ++) {
+
+						var html = template(response[i]);
+					
+						$('.container').append(html)
+					}
+				
+				},
+				//Call in case of request error
+				error: function (request, errorType, errorMessage){
+					alert('Error: ' + errorType + ', message: ' + errorMessage)
+					console.log(request);
+				}			
+			});
 	})
 	
 	})
+
 
 	$("#showMap").click(function(){
 		alert();
